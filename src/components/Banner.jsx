0e9a06dbab7e801961088125f10970ce0ab16f18
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-09-25 10:05:41 -0700

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = ({ onToggleClickableAreas }) => {
         <header className="relative bg-gradient-to-r from-red-700 via-yellow-100 to-red-700 p-6 mb-8 shadow-lg border-t-8 border-b-8 border-yellow-600">
             <div className="text-center">
                 <h1 className="text-4xl md:text-6xl font-bold font-serif text-black mb-4 tracking-wider">
-                    Shogun-G Project
+                    Shogun-G
                 </h1>
                 <p className="text-lg md:text-xl text-gray-700 font-light italic">
                     A Journey Through the World of the Shogun and Geisha
